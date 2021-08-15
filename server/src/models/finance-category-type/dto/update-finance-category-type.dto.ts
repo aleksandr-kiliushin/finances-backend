2a693e1c5b0baadb45013770d0017aad03cd6b23
Alexander <alexander-kilyushin@gmail.com>
2021-08-15 23:44:29 +0300

    n11: delete commented code

--- server/src/models/finance-category-type/dto/update-finance-category-type.dto.ts
+++ server/src/models/finance-category-type/dto/update-finance-category-type.dto.ts
@@ -1,13 +1,3 @@
-// import { Field, ID, InputType, PartialType } from '@nestjs/graphql'
-// import { CreateFinanceCategoryTypeDto } from './create-finance-category-type.dto'
-// // types
-// import { IFinanceCategoryType } from '#interfaces/finance'
-// @InputType()
-// export class UpdateFinanceCategoryTypeDto extends PartialType(CreateFinanceCategoryTypeDto) {
-// 	@Field(() => ID)
-// 	id: IFinanceCategoryType['id']
-// }
-
 import { IFinanceCategoryType } from '#interfaces/finance'
 import { Field, ID, InputType, PartialType } from '@nestjs/graphql'
 import { CreateFinanceCategoryTypeDto } from './create-finance-category-type.dto'
