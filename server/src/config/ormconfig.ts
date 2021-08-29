1c09e2a2f27a9d23137ea3dfa47eb6399bcb56ba
Alexander <alexander-kilyushin@gmail.com>
2021-08-29 12:55:02 +0300

    n15: add prod.env to server production docker-compose

--- server/src/config/ormconfig.ts
+++ server/src/config/ormconfig.ts
@@ -5,51 +5,34 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 dotenv.config({ path: path.join(__dirname, '..', '..', '..', 'config', process.env.MODE + '.env') })
 
-// const ormConfigDev: TypeOrmModuleOptions = {
-// 	cli: { migrationsDir: 'src/migration' },
-// 	database: 'finance',
-// 	entities: ['dist/**/*.entity{.js,.ts}'],
-// 	host: '127.0.0.1',
-// 	migrations: ['src/migration/*.ts'],
-// 	migrationsTableName: 'migration',
-// 	password: '123',
-// 	port: 5432,
-// 	ssl: false,
-// 	synchronize: true, //should set "false" for production.
-// 	type: 'postgres',
-// 	username: 'postgres',
-// }
-
-// const ormConfigProd: TypeOrmModuleOptions = {
-// 	cli: { migrationsDir: 'src/migration' },
-// 	database: process.env.DB_NAME,
-// 	entities: ['dist/**/*.entity{ .ts,.js}'],
-// 	host: process.env.DB_HOST,
-// 	migrations: ['src/migration/*.ts'],
-// 	migrationsTableName: 'migration',
-// 	password: process.env.DB_PASSWORD,
-// 	port: 5432,
-// 	ssl: false,
-// 	synchronize: false, // should be "false" for production.
-// 	type: 'postgres',
-// 	username: process.env.DB_USERNAME,
-// }
-
-// export default process.env.MODE === 'prod' ? ormConfigProd : ormConfigDev
+const ormConfigDev: TypeOrmModuleOptions = {
+	cli: { migrationsDir: 'src/migration' },
+	database: 'finance',
+	entities: ['dist/**/*.entity{.js,.ts}'],
+	host: '127.0.0.1',
+	migrations: ['src/migration/*.ts'],
+	migrationsTableName: 'migration',
+	password: '123',
+	port: 5432,
+	ssl: false,
+	synchronize: true, //should set "false" for production.
+	type: 'postgres',
+	username: 'postgres',
+}
 
 const ormConfigProd: TypeOrmModuleOptions = {
 	cli: { migrationsDir: 'src/migration' },
-	database: 'xwyksyfg',
+	database: process.env.DB_NAME,
 	entities: ['dist/**/*.entity{ .ts,.js}'],
-	host: 'hattie.db.elephantsql.com',
+	host: process.env.DB_HOST,
 	migrations: ['src/migration/*.ts'],
 	migrationsTableName: 'migration',
-	password: 'lVJxIokcu6b0e-IFty7LnTEsivCE0WSG',
+	password: process.env.DB_PASSWORD,
 	port: 5432,
 	ssl: false,
 	synchronize: false, // should be "false" for production.
 	type: 'postgres',
-	username: 'xwyksyfg',
+	username: process.env.DB_USERNAME,
 }
 
-export default ormConfigProd
+export default process.env.MODE === 'prod' ? ormConfigProd : ormConfigDev
