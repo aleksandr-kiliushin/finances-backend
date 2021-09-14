401869229ef001ebf9f388b9409c2b56d463ed90
Alexander <alexander-kilyushin@gmail.com>
2021-09-14 10:28:06 +0300

    n18: build mapSumToDate object

--- client/components/by-page/stats/chart.tsx
+++ client/components/by-page/stats/chart.tsx
@@ -16,7 +16,31 @@ export const Chart = () => {
 	useEffect(() => {
 		if (!data) return
 
-		const firstMaySum = 1_277_000
+		const { financeRecords } = data
+
+		const startDate = new Date(financeRecords[financeRecords.length - 1].date)
+		const endDate = new Date()
+
+		const mapSumToDate: Record<string, number> = {}
+
+		const currentDate = startDate
+		let currentSum = 1_277_000
+		while (currentDate <= endDate) {
+			const stringifiedCurrentDate = currentDate.toISOString().split('T')[0]
+
+			currentSum = financeRecords
+				.filter(record => record.date === stringifiedCurrentDate)
+				.reduce((dateSum, record) => {
+					if (record.category.type.name === 'expense') {
+						return (dateSum -= record.amount)
+					}
+					return (dateSum += record.amount)
+				}, currentSum)
+
+			mapSumToDate[stringifiedCurrentDate] = currentSum
+
+			currentDate.setDate(currentDate.getDate() + 1)
+		}
 
 		const canvas = canvasRef.current as HTMLCanvasElement | null
 
