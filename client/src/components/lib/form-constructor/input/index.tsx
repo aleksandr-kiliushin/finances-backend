b7cdaa91244fb39a9e8f5e1cf1e234f61b613d61
Alexander <alexander-kilyushin@gmail.com>
2021-10-11 22:58:49 +0300

    n24: Style: Login form

--- client/src/components/lib/form-constructor/input/index.tsx
+++ client/src/components/lib/form-constructor/input/index.tsx
@@ -1,7 +1,9 @@
 import { DetailedHTMLProps, ForwardedRef, forwardRef, InputHTMLAttributes } from 'react'
 
+import s from './index.module.css'
+
 const HookFormInput_ = (props: IProps, ref: ForwardedRef<HTMLInputElement>) => {
-	return <input ref={ref} {...props} />
+	return <input className={s.Input} ref={ref} {...props} />
 }
 
 export const HookFormInput = forwardRef(HookFormInput_)
