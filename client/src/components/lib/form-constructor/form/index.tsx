7daa92881ded965e7260f5b07163400b45f4fb83
Alexander <alexander-kilyushin@gmail.com>
2021-10-11 23:03:17 +0300

    n24: fix: pass rest props to <form/>

--- client/src/components/lib/form-constructor/form/index.tsx
+++ client/src/components/lib/form-constructor/form/index.tsx
@@ -3,8 +3,12 @@ import { DetailedHTMLProps, FormHTMLAttributes } from 'react'
 // Styles
 import s from './index.module.css'
 
-export const Form = ({ children }: IProps) => {
-	return <form className={s.Form}>{children}</form>
+export const Form = ({ children, ...rest }: IProps) => {
+	return (
+		<form className={s.Form} {...rest}>
+			{children}
+		</form>
+	)
 }
 
 type IProps = DetailedHTMLProps<FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>
