72d11fce804ad7d404e069511a6ccb294c7cd89f
Alexander <alexander-kilyushin@gmail.com>
2021-10-15 10:56:29 +0300

    n26: refactor: change order in tsconfig.json, remove commented code in webpack.module.css

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,25 +23,6 @@ module.exports = {
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
 			},
-			// {
-			// 	test: /\.css$/,
-			// 	use: [
-			// 		'style-loader',
-			// 		{
-			// 			loader: 'css-loader',
-			// 			options: {
-			// 				importLoaders: 1,
-			// 				modules: true,
-			// 			},
-			// 		},
-			// 	],
-			// 	include: /\.module\.css$/,
-			// },
-			// {
-			// 	test: /\.css$/,
-			// 	use: ['style-loader', 'css-loader'],
-			// 	exclude: /\.module\.css$/,
-			// },
 		],
 	},
 	output: {
