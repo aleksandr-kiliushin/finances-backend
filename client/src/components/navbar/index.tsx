9a236b7fa6f10cc96be228ebf14ff86a27dbc8fd
Alexander <alexander-kilyushin@gmail.com>
2021-10-15 12:48:42 +0300

    n26: fix issue with <a/> inside <Link/>

---
+++ client/src/components/navbar/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useLocation } from 'react-router'
+import cx from 'classnames'
+
+// Components
+import { ISvgProps, Svg } from '#components/svg'
+
+// Styles
+import s from './index.module.css'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+	const { pathname } = useLocation()
+
+	if (!localStorage.authToken) return null
+
+	const sectionsData: ISection[] = [
+		{ path: '/', svgName: 'home' },
+		{ path: '/records', svgName: 'box' },
+		{ path: '/stats', svgName: 'chart-up' },
+		{ path: '/trash', svgName: 'trash-can' },
+		{ path: '/settings', svgName: 'gear' },
+		{ path: '/login', svgName: 'person' },
+	]
+
+	const sectionsDataForLayout = sectionsData.map(({ path, svgName }) => ({
+		className: cx({ [s.SectionLink]: true, [s.ActiveSectionLink]: pathname === path }),
+		path,
+		svgName,
+	}))
+
+	return (
+		<nav className={s.Navbar}>
+			{sectionsDataForLayout.map(({ path, className, svgName }) => (
+				<Link className={className} key={path} to={path}>
+					<Svg name={svgName} />
+				</Link>
+			))}
+		</nav>
+	)
+}
+
+interface ISection {
+	path: string
+	svgName: ISvgProps['name']
+}
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,8 +23,7 @@ module.exports = {
 		],
 	},
 	output: {
-		chunkFilename: '[id].[chunkhash].js',
-		filename: '[name].js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 		publicPath: '/',
 		sourceMapFilename: '[name].js.map',
