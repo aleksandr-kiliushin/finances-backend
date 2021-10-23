ad63acbfa0d45777f6564abcc4f9e0ad4c373a57
Alexander <alexander-kilyushin@gmail.com>
2021-10-23 13:55:28 +0300

    n32: fix button test

--- client/src/components/Button/__test__/index.test.tsx
+++ client/src/components/Button/__test__/index.test.tsx
@@ -1,9 +1,7 @@
-/**
- * @jest-environment jsdom
- */
+/** @jest-environment jsdom */
 
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 
 // Components
 import { Button } from '#components/Button'
@@ -12,16 +10,17 @@ describe('<Button/> component.', () => {
 	test('Passes text to <Button/> children.', () => {
 		const buttonChildren = 'Button text'
 
-		render(<Button>{buttonChildren}</Button>)
+		const { getByText } = render(<Button>{buttonChildren}</Button>)
 
-		expect(screen.getByText(buttonChildren)).toBeDefined()
+		expect(getByText(buttonChildren)).toBeDefined()
 	})
 
 	test('onClick callback gets called when a click happens.', () => {
 		const buttonCallback = jest.fn()
 
-		render(<Button onClick={buttonCallback}>Click me</Button>)
-		fireEvent.click(screen.getByRole('button'))
+		const { getByRole } = render(<Button onClick={buttonCallback}>Click me</Button>)
+
+		fireEvent.click(getByRole('button'))
 
 		expect(buttonCallback).toBeCalledTimes(1)
 	})
