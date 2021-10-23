4fab895c2e67cbea9d61c9499f0ae9754070add6
Alexander <alexander-kilyushin@gmail.com>
2021-10-23 13:36:49 +0300

    n32: Finish tests for <Button/>

--- client/src/components/Button/__test__/index.test.tsx
+++ client/src/components/Button/__test__/index.test.tsx
@@ -3,17 +3,26 @@
  */
 
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 // Components
 import { Button } from '#components/Button'
 
-test('Passes text to <Button/> children.', () => {
-	const buttonChildren = 'Button text'
+describe('<Button/> component.', () => {
+	test('Passes text to <Button/> children.', () => {
+		const buttonChildren = 'Button text'
 
-	render(<Button>{buttonChildren}</Button>)
+		render(<Button>{buttonChildren}</Button>)
 
-	expect(screen.getByText(buttonChildren)).toBeDefined()
+		expect(screen.getByText(buttonChildren)).toBeDefined()
+	})
+
+	test('onClick callback gets called when a click happens.', () => {
+		const buttonCallback = jest.fn()
+
+		render(<Button onClick={buttonCallback}>Click me</Button>)
+		fireEvent.click(screen.getByRole('button'))
+
+		expect(buttonCallback).toBeCalledTimes(1)
+	})
 })
-
-export default {}
