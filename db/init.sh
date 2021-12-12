70c8609f139878fd27cf46bcb0f3ab8d693fd34f
Alexander <alexander.kilyushin@gmail.com>
2021-12-12 23:05:42 +0300

    n37: feat: Init db with compose.dev.yml and ctrl + V from txt

--- db/init.sh
+++ db/init.sh
@@ -7,12 +7,4 @@ curl -u :$REMOTE_API_KEY --output app/dumps/dumps.json https://api.elephantsql.c
 DUMP_URL=$(jq 'max_by(.backup_date) | .url' app/dumps/dumps.json -r);
 
 # Downloading the last dump.
-echo "curl $DUMP_URL --output /app/dumps/dump.lzo"
-
-# Create an empty db.
-# echo "CREATE DATABASE $LOCAL_DB ENCODING 'UTF-8';" | docker exec -i $LOCAL_SERVER psql -U postgres;
-
-# echo "populating local db from the downloaded dump";
-# echo "lzop -cd /db/dump.lzo | psql -U postgres $LOCAL_DB" | docker exec -i $LOCAL_SERVER bash;
-
-# echo "\l" | docker exec -i $LOCAL_SERVER psql -U postgres;
+curl $DUMP_URL --output /app/dumps/dump.lzo
