bf672837e7c8bda62f51645d5b41a81b6a2de629
Alexander <alexander.kilyushin@gmail.com>
2021-12-18 12:51:41 +0300

    n37: DB initialized by connecting to container and running app/init.sh

--- db/init.sh
+++ db/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# After the DB container has started, run this script.
+# From host machine execute: docker exec -it finance-db /bin/sh
+# Inside the DB container execute: bash /app/init.sh
+
 # Loading dump list from remote server to the host machine.
 curl -u :$REMOTE_API_KEY --output app/dumps/dumps.json https://api.elephantsql.com/api/backup?db=$REMOTE_DB_NAME;
 
@@ -8,3 +12,9 @@ DUMP_URL=$(jq 'max_by(.backup_date) | .url' app/dumps/dumps.json -r);
 
 # Downloading the last dump.
 curl $DUMP_URL --output /app/dumps/dump.lzo
+
+# Create a new clear DB for out app.
+psql -U postgres -c "CREATE DATABASE finance ENCODING 'UTF-8';";
+
+# Restore DB from the downloaded dump.
+lzop -cd /app/dumps/dump.lzo | psql -U postgres finance;
