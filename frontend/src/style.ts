856cec23a40add45cc3adb51a7edafe2101cf002
Alexander <alexander.kilyushin@gmail.com>
2021-12-22 00:09:28 +0300

    n42: Remove old global styles

--- frontend/src/style.ts
+++ frontend/src/style.ts
@@ -9,39 +9,13 @@ export enum Color {
 const globalStyles = css`
   html,
   body {
+    margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
       Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
   * {
-    margin: 0;
-    padding: 0;
     box-sizing: border-box;
-  }
-
-  svg {
-    fill: var(--soft-black);
-  }
-
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  h2 {
-    font-size: 1.75rem;
-  }
-
-  h3 {
-    font-size: 1.5rem;
-  }
-
-  h4 {
-    font-size: 1.25rem;
   }
 `
 
