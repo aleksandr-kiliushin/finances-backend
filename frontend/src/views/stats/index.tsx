73819eabd0e0a27bbefdc746a26b61993926ce6f
Alexander <alexander.kilyushin@gmail.com>
2021-12-22 00:02:56 +0300

    n42: Restore desktop chart

--- frontend/src/views/stats/index.tsx
+++ frontend/src/views/stats/index.tsx
@@ -31,6 +31,84 @@ export const Stats = () => {
     Chart.register(...registerables)
 
     const endDate = new Date()
+
+    const isDesktop = document.body.clientWidth > 768
+
+    if (isDesktop) {
+      const mapSumToDate: {
+        x: string
+        y: number
+      }[] = []
+
+      const currentDate = new Date(items[0].date)
+      let currentSum = 1_277_000
+      while (currentDate <= endDate) {
+        const stringifiedCurrentDate = currentDate.toISOString().split('T')[0]
+        currentSum = items
+          .filter((record) => record.date === stringifiedCurrentDate)
+          .reduce((dateSum, record) => {
+            if (record.category.type.name === 'expense') {
+              return (dateSum -= record.amount)
+            }
+            return (dateSum += record.amount)
+          }, currentSum)
+
+        mapSumToDate.push({
+          x: stringifiedCurrentDate,
+          y: currentSum,
+        })
+
+        currentDate.setDate(currentDate.getDate() + 1)
+      }
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              borderColor: 'darkgreen',
+              borderWidth: 1,
+              data: Object.values(mapSumToDate),
+              fill: true,
+              label: 'Your finance, RUB',
+              pointBorderColor: 'darkgreen',
+              pointRadius: 0,
+            },
+          ],
+        },
+        options: {
+          maintainAspectRatio: false, // So chart takes the available height.
+          scales: {
+            x: {
+              ticks: {
+                callback: (tickValue) => {
+                  if (typeof tickValue !== 'number') return 'error'
+                  const timeStamp = new Date(items[0].date).setDate(
+                    currentDate.getDate() + tickValue,
+                  )
+                  return new Date(timeStamp).toLocaleString('default', {
+                    month: 'short',
+                    year: '2-digit',
+                  })
+                },
+                maxRotation: 90,
+                minRotation: 90,
+              },
+            },
+            y: {
+              beginAtZero: true,
+              ticks: {
+                callback: (tickValue) =>
+                  typeof tickValue === 'number' ? tickValue / 1_000_000 : 'error',
+                padding: 1,
+              },
+            },
+          },
+        },
+      })
+
+      return
+    }
+
     const mapSumToDate: {
       x: number
       y: string
