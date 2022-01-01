bef0a8618b744085ec194db4d46d27356a0a319f
Alexander <alexander.kilyushin@gmail.com>
2022-01-01 15:46:45 +0300

    n0: Rename docker containers

--- db/init.sh
+++ db/init.sh
@@ -3,10 +3,10 @@
 # After the DB container has started, run this script to initialize DB.
 
 # Variant 1:
-# From your host machine execute: echo "bash /app/init.sh" | docker exec -i finance-db bash;
+# From your host machine execute: echo "bash /app/init.sh" | docker exec -i finances-db bash;
 
 # Variant 2:
-# From your host machine execute: docker exec -it finance-db /bin/sh
+# From your host machine execute: docker exec -it finances-db /bin/sh
 # Inside the DB container execute: bash /app/init.sh
 
 # Loading dump list from remote server to the container.
@@ -19,7 +19,7 @@ DUMP_URL=$(jq 'max_by(.backup_date) | .url' /var/app/dumps/dumps.json -r);
 curl $DUMP_URL --output /var/app/dumps/dump.lzo
 
 # Create a new clear DB for out app.
-psql -U postgres -c "CREATE DATABASE finance ENCODING 'UTF-8';";
+psql -U postgres -c "CREATE DATABASE finances ENCODING 'UTF-8';";
 
 # Restore DB from the downloaded dump.
-lzop -cd /var/app/dumps/dump.lzo | psql -U postgres finance;
+lzop -cd /var/app/dumps/dump.lzo | psql -U postgres finances;
