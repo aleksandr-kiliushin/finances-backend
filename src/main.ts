981051daa34f10dcf4582e82c657f5f257b8aba5
Alexander <alexander.kilyushin@gmail.com>
2022-01-01 18:29:17 +0300

    n45: Add dockerfile.prod, remove cors config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from 'src/app.module'
-import corsConfig from 'src/config/cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors(corsConfig)
+  app.enableCors()
   app.setGlobalPrefix('api')
   await app.listen(process.env.BACKEND_PORT)
 }
