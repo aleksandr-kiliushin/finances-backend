8122dda07c55500ee659592ee5a28ddcb6811cd0
Alexander <alexander.kilyushin@gmail.com>
2022-01-01 21:21:41 +0300

    n45: process.env.BACKEND_PORT ?? 3080

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix('api')
-  await app.listen(process.env.BACKEND_PORT)
+  await app.listen(process.env.BACKEND_PORT ?? 3080)
 }
 
 bootstrap()
