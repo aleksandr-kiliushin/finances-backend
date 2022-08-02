43b8485b7979af05411b10ee3fbe82ca48e9f576
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-03 00:09:09 +0400

    n48: Test when password is not valid and when password is valid

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,19 @@
 const { execSync } = require("child_process")
 
-beforeEach(() => {
+const registerAUser = async ({ password, username }) => {
+  await fetch("http://localhost:3080/api/user", {
+    body: JSON.stringify({ password, username }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  })
+}
+
+beforeEach(async () => {
   execSync('echo "bash /var/app/fill-db-in-with-testing-data.sh" | docker exec -i personal-app-database bash;')
+  await registerAUser({ username: "john-doe", password: "john-doe-password" })
+  await registerAUser({ username: "jessica-stark-doe", password: "jessica-stark-password" })
 })
 
 afterAll(() => {
