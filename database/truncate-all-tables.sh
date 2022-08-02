2b9467d1e92f479d65ecbf41aaf562cde699b6fa
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-02 18:30:50 +0400

    n48: Make sql command multiline in bash script

--- database/truncate-all-tables.sh
+++ database/truncate-all-tables.sh
@@ -2,10 +2,11 @@
 
 # This script will be executed automatically before each test.
 
-psql $DATABASE_NAME $DATABASE_USERNAME -c "\
-TRUNCATE \
-finance_category, \
-finance_category_type, \
-finance_record, \
-\"user\" \
-CASCADE";
+psql $DATABASE_NAME $DATABASE_USERNAME << EOF
+  TRUNCATE
+  finance_category,
+  finance_category_type,
+  finance_record,
+  "user"
+  CASCADE;
+EOF
