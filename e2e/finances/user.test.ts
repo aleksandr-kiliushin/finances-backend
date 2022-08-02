a6a335d0730aabba1b0b298f60774a69f5ed4aea
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-02 11:59:01 +0400

    n48: Adds first real test for login

--- e2e/finances/user.test.ts
+++ e2e/finances/user.test.ts
@@ -1,11 +1,20 @@
-describe("user", () => {
-  test("I am 25 y. o.", () => {
-    const myAge = 25
-    expect(myAge).toEqual(25)
-  })
-
-  test("I live in Georgia.", () => {
-    const theCountryILiveIn = "Georgia"
-    expect(theCountryILiveIn).toEqual("Georgia")
+describe("Login", () => {
+  it("responds that user is not found", async () => {
+    const response = await fetch("http://localhost:3080/api/login", {
+      body: JSON.stringify({
+        username: "unexisting-username",
+        password: "some-password",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+    const responseData = await response.json()
+    expect(responseData).toEqual({
+      error: "Not Found",
+      message: "User not found.",
+      statusCode: 404,
+    })
   })
 })
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-beforeAll(() => {})
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-afterEach(() => {})
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-afterAll(() => {})
+// beforeAll(() => {})
+// afterEach(() => {})
+// afterAll(() => {})
