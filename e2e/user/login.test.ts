2899487119b4d7d9e59cfd579c1c7a268ae37872
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-02 18:03:42 +0400

    n48: Adds invalid password test

--- e2e/user/login.test.ts
+++ e2e/user/login.test.ts
@@ -2,8 +2,8 @@ describe("Login", () => {
   it("responds that user is not found", async () => {
     const response = await fetch("http://localhost:3080/api/login", {
       body: JSON.stringify({
-        username: "sasha",
-        password: "s3cret",
+        username: "nonexistent-username",
+        password: "some-password",
       }),
       headers: {
         "Content-Type": "application/json",
@@ -16,6 +16,25 @@ describe("Login", () => {
       error: "Not Found",
       message: "User not found.",
       statusCode: 404,
+    })
+  })
+
+  it("responds 'unauthorized' if the user is found but password is invalid", async () => {
+    const response = await fetch("http://localhost:3080/api/login", {
+      body: JSON.stringify({
+        username: "john-doe",
+        password: "invalid-password",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+
+    expect(response.status).toEqual(401)
+    expect(await response.json()).toEqual({
+      message: "Unauthorized",
+      statusCode: 401,
     })
   })
 })
